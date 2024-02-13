f884ab74e1a2f004dcabe97b1b825efd4e3a7ce8
Brandon <rbrandon087@gmail.com>
2024-02-12 22:52:14 -0800

    added my news sections in my homepage

--- client/news/src/components/Navbar.tsx
+++ client/news/src/components/Navbar.tsx
@@ -1,33 +1,53 @@
-import React from 'react';
-import '../components/Navbar.css';
+import React from "react";
+import "../components/Navbar.css";
 
 export interface INavbarProps {
-    name: string;
-};
- 
-const Navbar : React.FunctionComponent<INavbarProps> = ({ name }) => {
-    return (
-        <div>
-            <h1>{name}</h1>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-custom">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">W.</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav ms-auto">
-        <a className="nav-link" href="/">Home</a>
-        <a className="nav-link" href="/New">New</a>
-        <a className="nav-link" href="/Popular">Popular</a>
-        <a className="nav-link" href="/Trending">Trending</a>
-        <a className="nav-link" href="Categories">Categories</a>
-      </div>
-    </div>
-  </div>
-</nav>
-        </div>
-    );
+  name: string;
 }
- 
+
+const Navbar: React.FunctionComponent<INavbarProps> = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-custom">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            W.
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav ms-auto">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+              <a className="nav-link" href="/New">
+                New
+              </a>
+              <a className="nav-link" href="/Popular">
+                Popular
+              </a>
+              <a className="nav-link" href="/Trending">
+                Trending
+              </a>
+              <a className="nav-link" href="Categories">
+                Categories
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
 export default Navbar;
--- client/news/src/pages/Home.tsx
+++ client/news/src/pages/Home.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Shapes from '../images/shapes.jpg'
-import '../styles/Home.css';
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import Shapes from "../images/shapes.jpg";
+import "../styles/Home.css";
 
 export interface IHomeProps {
-    name: string;
-    paragraph: string;
-};
- 
-const Home : React.FunctionComponent<IHomeProps> = ({ name, paragraph }) => {
-    return (
-        <div>
-            <h1> {name}</h1>
-            <p>{paragraph}</p>
-            <img className='shapes-image' src={Shapes}
-            alt="Shape image"
-            />
-        </div>
-    );
+  name: string;
+  paragraph: string;
 }
- 
+
+const Home: React.FunctionComponent<IHomeProps> = ({ name, paragraph }) => {
+  return (
+    <div>
+      <h1> {name}</h1>
+      <p>{paragraph}</p>
+      <img className="shapes-image" src={Shapes} alt="Shape image" />
+      <div className="alert alert-success" role="alert">
+        <h4 className="alert-heading">New</h4>
+        <p>
+          Aww yeah, you successfully read this important alert message. This
+          example text is going to run a bit longer so that you can see how
+          spacing within an alert works with this kind of content.
+        </p>
+        <p className="mb-0">
+          Whenever you need to, be sure to use margin utilities to keep things
+          nice and tidy.
+        </p>
+        <p className="mb-0">
+          Whenever you need to, be sure to use margin utilities to keep things
+          nice and tidy.
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
