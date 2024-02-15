85adac1b1e2ab86f2be10b65b30ba19ae14a7fe7
Brandon <rbrandon087@gmail.com>
2024-02-14 22:55:18 -0800

    adjusting my css for my content

--- client/news/src/pages/Home.tsx
+++ client/news/src/pages/Home.tsx
@@ -14,29 +14,45 @@ const Home: React.FunctionComponent<IHomeProps> = ({ name, paragraph }) => {
   return (
     <div>
       <h1> {name}</h1>
-      {/* Shapes image above*/}
+      {/* Shapes image*/}
       <img className="shapes-image" src={Shapes} alt="Shape image" />
       {/* Text  info here */}
-      <section>
-        <p className="big-text">The Bright Future of Web 3.0? {paragraph}</p>
-        <p className="big-info"></p>
-      </section>
+      <div className="content">
+        <section>
+          <p className="big-text">The Bright Future of Web 3.0? {paragraph}</p>
+          <p className="big-info"></p>
+        </section>
+      </div>
+      <div className="button-container">
+        {/* Button  */}
+        <button type="button" className="btn btn-primary">
+          Read More
+        </button>
+      </div>
       {/* Info section here*/}
-      <div className="alert alert-success" role="alert">
-        <h4 className="alert-heading">New</h4>
-        <p>
-          Aww yeah, you successfully read this important alert message. This
-          example text is going to run a bit longer so that you can see how
-          spacing within an alert works with this kind of content.
-        </p>
-        <p className="mb-0">
-          Whenever you need to, be sure to use margin utilities to keep things
-          nice and tidy.
-        </p>
-        <p className="mb-0">
-          Whenever you need to, be sure to use margin utilities to keep things
-          nice and tidy.
-        </p>
+      <div>
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">New</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <p className="p-2">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+          <p className="p-2">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+
+          <p className="p-3">
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+        </div>
       </div>
     </div>
   );
