e6c3f1cb00e483dd576452d24fe0ecc657c44263
Brandon <rbrandon087@gmail.com>
2024-02-15 22:56:05 -0800

    added my bottom content and styling still need to fix my navbar as well

--- client/news/src/pages/Home.tsx
+++ client/news/src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Shapes from "../images/shapes.jpg";
+import Controller from "../images/controller.jpg";
+import Keyboard from "../images/keyboard.jpg";
+import Retro from "../images/retro.jpg";
 import "../styles/Home.css";
 
 export interface IHomeProps {
@@ -13,6 +16,7 @@ export interface IHomeProps {
 const Home: React.FunctionComponent<IHomeProps> = ({ name, paragraph }) => {
   return (
     <div>
+      {/* image and new alert here*/}
       <div className="main-content">
         <img className="shapes-image" src={Shapes} alt="Shape image" />
         <div className="alert alert-success" role="alert">
@@ -38,6 +42,7 @@ const Home: React.FunctionComponent<IHomeProps> = ({ name, paragraph }) => {
           </p>
         </div>
       </div>
+      {/* text information here*/}
       <div className="text-container">
         <h1 className="main-title"> The Bright Future of Web 3.0? {name}</h1>
         <p className="text">
@@ -48,6 +53,28 @@ const Home: React.FunctionComponent<IHomeProps> = ({ name, paragraph }) => {
         <button type="button" className="btn btn-primary btn-lg">
           Large button
         </button>
+      </div>
+      {/* bottom images here*/}
+      <div className="bottom-content">
+        <div className="card mb-3 no-border">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src={Retro}
+                className="img-fluid rounded-start retro"
+                alt="Retro PC"
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Reviving Retro PCs</h5>
+                <p className="card-text">
+                  What happens when old PCs are given modern upgrades?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
