2acae1609e71ed1964d649506a97d9ddd3b5ad77
Brandon <rbrandon087@gmail.com>
2024-02-17 23:22:26 -0800

    made a line between my new alert

--- client/news/src/pages/Home.tsx
+++ client/news/src/pages/Home.tsx
@@ -21,21 +21,21 @@ const Home: React.FunctionComponent<IHomeProps> = ({ name, paragraph }) => {
         <img className="shapes-image" src={Shapes} alt="Shape image" />
         <div className="alert alert-success" role="alert">
           <h4 className="alert-heading">New</h4>
-          <p>
+          <p className="p-2 p-with-line">
             Aww yeah, you successfully read this important alert message. This
             example text is going to run a bit longer so that you can see how
             spacing within an alert works with this kind of content.
           </p>
-          <p className="p-2">
+          <p className="p-2 p-with-line">
             Whenever you need to, be sure to use margin utilities to keep things
             nice and tidy.
           </p>
-          <p className="p-2">
+          <p className="p-2 p-with-line">
             Whenever you need to, be sure to use margin utilities to keep things
             nice and tidy.
           </p>
 
-          <p className="p-3">
+          <p className="p-2 p-with-line">
             Aww yeah, you successfully read this important alert message. This
             example text is going to run a bit longer so that you can see how
             spacing within an alert works with this kind of content.
